e95a811edb1bc4d3a4dcf3b2883e4f8d8cce6271
HyunJeong Kim <betty00507@dgu.ac.kr>
2024-08-28 09:33:06 +0900

    add components

---
+++ components/Customer.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container1">
+      <div class="inform"><b>고객성명 : </b></div>
+      <input type="text" id="search_name"><br>
+      <button id="filter">조건 검색</button><br>
+      <button id="all">전체 검색</button>
+      <fieldset class="names">
+        <input type="radio" name="customers" value="윤하나" v-model="myPicked" checked>윤하나<br>
+        <input type="radio" name="customers" value="강해라" v-model="myPicked">강해라<br>
+        <input type="radio" name="customers" value="나란다" v-model="myPicked">나란다<br>
+        <input type="radio" name="customers" value="담나라" v-model="myPicked">담나라<br>
+        <input type="radio" name="customers" value="라면다" v-model="myPicked">라면다<br>
+        <input type="radio" name="customers" value="박석일" v-model="myPicked">박석일<br>
+        <input type="radio" name="customers" value="이성경" v-model="myPicked">이성경<br>
+        <input type="radio" name="customers" value="김임시" v-model="myPicked">김임시
+        <!-- <p>{{ myPicked }}</p> -->
+        <!-- {{CustomerData[0]}} -->
+      </fieldset>
+  </div>
+
+  <InformationVue :CustomerData="CustomerData" :myPicked="myPicked"></InformationVue>
+
+</template>
+
+<script>
+
+import CustomerData from '../assets/customer.js';
+import InformationVue from '../components/Information.vue';
+
+export default {
+    name: 'CustomerVue',
+    data() {
+      return {
+        CustomerData : CustomerData,
+        myPicked:'윤하나',
+      }
+    },
+    props: {
+        // member : Object,
+    },
+    components : {
+      InformationVue,
+    },
+
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ components/Information.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container2">
+      <ul class="information">
+        <label for="c_date">**작성일자:</label>
+        <input type="text" id="c_date" v-model="localMember.create_date">
+        <br>
+        <label for="c_name">*고객명:</label>
+        <input type="text" id="c_name" v-model="localMember.cus_name">
+        <br>
+        <label for="c_jm_number">*실명번호:</label>
+        <input type="text" id="c_jm_number" v-model="localMember.jumin_id"> 
+        <br>
+        <label for="c_email">*E-mail:</label>
+        <input type="email" id="c_email" v-model="localMember.email"> 
+        <br>
+        <label for="c_home_call">전화번호:</label>
+        <input type="text" id="c_home_call" v-model="localMember.tel"> 
+        <br>
+        <label for="c_phone_call">*핸드폰 번호:</label>
+        <input type="text" id="c_phone_call" v-model="localMember.phone"> 
+        <br>
+        <label for="job">*직업:</label>
+        <input type="text" id="job" v-model="localMember.job"> 
+        <br>
+        <label for="c_address">주소:</label>
+        <input type="text" id="c_address" v-model="localMember.addr"> 
+        <br>
+        
+        <br>
+        <label for="m_name">관리담당자:</label>
+        <select id="m_name">
+          <option value="강하게">강하게</option>
+          <option value="나대리">나대리</option>
+          <option value="고과장">고과장</option>
+        </select>
+        <br>
+        <label for="dept">**부서:</label>
+        <input type="text" id="dept" > 
+        <br>
+        <label for="position">**직위:</label>
+        <input type="text" id="position" > 
+        <br>
+        <label for="m_phone">**연락처:</label>
+        <input type="text" id="m_phone" > {{ member }}
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InformationVue',
+    props : {
+      CustomerData: Array,
+      myPicked: String,
+      
+    },
+    computed: {
+      localMember() {
+        // Ensure CustomerData is defined and is an array
+        if (Array.isArray(this.CustomerData)) {
+          return this.CustomerData.find(customer => customer.cus_name === this.myPicked) || {};
+        }
+        return {}; // Return an empty object if CustomerData is not an array
+      }
+    },
+    // data() {
+    //   return {
+    //     localMember: this.computedMember(this.myPicked)  // Initialize localMember with the member data
+    //   }
+    // },
+
+}
+</script>
+
+<style>
+
+</style>
